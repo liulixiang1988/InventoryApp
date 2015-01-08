a6348fbd0fb0d970e79af737ce84b6e7a3d211bf
liulixiang1988 <liulixiang1988@gmail.com>
2015-01-08 12:52:37 +0800

    添加出库项

---
+++ app/src/main/java/tgit/inventory/ui/custom/InvOutItemsListAdapter.java
@@ -0,0 +1,31 @@
+package tgit.inventory.ui.custom;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by liulixiang on 2015/1/8.
+ * Description:
+ */
+public class InvOutItemsListAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
